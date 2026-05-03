bd91805dd37fdbe7203b09af990824aaa4078868
vanita006 <cherryveervansh@gmail.com>
2026-05-04 03:34:41 +0530

    4

--- src/app/api/discount/route.ts
+++ src/app/api/discount/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { isDiscountActive } from "@/lib/helpers";
+import { getOrCreateSettings } from "@/lib/helpers";
 import { connectDB } from "@/lib/db";
 
 export async function GET() {
